9089e40b584cbb03b8fe152d6aea53d47b6d2ece
ATRI <A@T.RI>
2023-12-21 18:22:03 +0800

    红移和高斯模糊数据增强方法上传

--- nearest.py
+++ nearest.py
@@ -18,8 +18,6 @@ def resize_image(image, target_size=(27, 42)):
     # cv2.imshow('Resized Image', r)
     # cv2.waitKey(0)
 
-from PIL import Image
-
 ROOTDIR = Path(r'D:\SETU\shiranai\Assets')
 LABEL = ROOTDIR / 'label'
 OUTDIR = ROOTDIR / 'nearest'
@@ -40,13 +38,36 @@ def tokenize_img(imgpath: str) -> Tuple[np.ndarray, np.ndarray]:
     # ret, img = cv2.threshold(img, 175, 255, cv2.THRESH_BINARY_INV)
     return img, cimg
 
+def blur_augment(img: np.ndarray, k=3) -> np.ndarray:
+    # 高斯模糊
+    return cv2.GaussianBlur(img, (k, k), 0)
+
+def red_augment(img: np.ndarray, w=0.2) -> np.ndarray:
+    # 创建一个红色的滤镜，数值中红色分量被设置得较高
+    red_filter = np.full_like(img, (0, 0, 255))
+
+    # 通过加权合并原图与红色滤镜来给图像加上红色滤镜
+    # cv2.addWeighted参数：(第一个图像, 第一个图像的权重, 第二个图像, 第二个图像的权重, gamma值)
+    # 调整权重来控制红色滤镜的强度
+    red_image = cv2.addWeighted(img, 1.-w, red_filter, w, 0)
+
+    return red_image
+
 if __name__ == '__main__':
     ds = {} # 训练样本空间
     now = datetime.datetime.now()
     for label in os.listdir(LABEL):
         for i in os.listdir(LABEL / label):
-            imgpath = LABEL / label / i # 本目录下保证都是jpg文件
+            imgpath = LABEL / label / i
             tokenized, original = tokenize_img(str(imgpath.absolute()))
+            cv2.imshow('original', original)
+            # for i in range(16,25,2):
+            #     redded = red_augment(original, i / 100)
+            #     cv2.imshow('redded-'+ str(i), redded)
+            # for i in range(3, 13, 2):
+            #     blurred = blur_augment(original, i)
+            #     cv2.imshow('blurred-'+ str(i), blurred)
+            cv2.waitKey()
             ds.setdefault(label, []).append((tokenized, original))
     print('train set loaded in', datetime.datetime.now() - now)
     shutil.rmtree(OUTDIR)
